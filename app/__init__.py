ae0f48e056f378a128220e4c3d293608034eee3e
redelephant <396169330@qq.com>
2020-06-30 10:46:29 +0800

    添加配置文件

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,15 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import config
 
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
     db.app = app
     db.init_app(app)
+
     return app
---
+++ config.py
@@ -0,0 +1,43 @@
+#!python3.6
+# -*- coding: utf-8 -*-
+# @Introduction : 圈服务相关配置文件
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    # 秘钥
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'redelephant_circle'
+    # 配置
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 500
+
+    def init_app(self):
+        pass
+
+
+class DevelopConfig(Config):
+
+    DEBUG = True
+
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@127.0.0.1/circlecharset=utf8mb4'
+
+
+class TestingConfig(Config):
+    pass
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    'development': DevelopConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopConfig
+}
